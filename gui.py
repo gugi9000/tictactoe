0390084d141b2f6b5cc255f31f73ad2007391084
Bjarke Sørensen <bs@wasd.dk>
2019-01-12 01:45:11 +0100

    doing some boarddrawing

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
-# import the pygame module, so you can use it
 import pygame
- 
+from time import sleep
+
 # define a main function
 def main():
      
@@ -9,26 +9,64 @@ def main():
     # load and set the logo
     logo = pygame.image.load("logo32x32.png")
     pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
-     
-    # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240,180))
-     
-    # define a variable to control the main loop
+    pygame.display.set_caption("super awesome tic-tac-toe")
+    
+    screen_width = 800
+    screen_height = 600
+    BLACK = (  0,   0,   0)
+    WHITE = (255, 255, 255)
+    BLUE =  ( 70,  70, 200)
+    GREEN = (  0, 255,   0)
+    RED =   (200,  30,  30)
+    xpos = 50
+    ypos = 50
+    step_x = 10
+    step_y = 10
+    FPS = 30
+
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    background = pygame.Surface(screen.get_size())
+    background.fill(BLACK) 
+    background = background.convert()
+
+    pygame.draw.line(screen, BLUE, [267, 0],[267, 600], 20)
+    pygame.draw.line(screen, BLUE, [534, 0],[534, 600], 20)
+    pygame.draw.line(screen, BLUE, [0, 200],[800, 200], 20)
+    pygame.draw.line(screen, BLUE, [0, 400],[800, 400], 20)
+
+    pygame.draw.circle(screen, RED, [130 , 90], 90, 10)
+    # image = pygame.image.load("logo32x32.png")
+
+    clock = pygame.time.Clock()
+    pygame.mouse.set_visible(False)
+    # screen.blit(image, (50,50)) 
+    
+
+    
     running = True
      
-    # main loop
     while running:
-        # event handling, gets all event from the event queue
+
+        # if xpos>screen_width-64 or xpos<0:
+        #     step_x = -step_x
+        # if ypos>screen_height-64 or ypos<0:
+        #     step_y = -step_y
+        # xpos += step_x
+        # ypos += step_y
+        
+        # screen.blit(image, (xpos, ypos))
+        pygame.display.flip()
+        milliseconds = clock.tick(FPS)
+        print(milliseconds)
+
         for event in pygame.event.get():
-            # only do something if the event is of type QUIT
+            
             if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
                 running = False
-     
-     
-# run the main function only if this module is executed as the main script
-# (if you import this as a module then nothing is executed)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+    pygame.mouse.set_visible(True)
 if __name__=="__main__":
-    # call the main function
     main()
